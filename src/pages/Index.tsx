7b7e418c0fdfcb12b1115ed2f3368601fc095652
johanna-boateng <favoritegirlbeautyhub20@gmail.com>
2025-03-11 21:04:12 +0000

    name change

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,9 +4,9 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Benefits from "@/components/Benefits";
-import Testimonials from "@/components/Testimonials";
+import Testimonials from "@/components/testimonials";
 import CallToAction from "@/components/CallToAction";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer";
 
 const Index = () => {
   useEffect(() => {
